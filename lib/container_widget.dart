7d8663e1db53dbcaf407b5d7a2980c9714fb907a
GeraAnggaraPutra <gera_098@smkassalaambandung.sch.id>
2024-01-22 14:43:16 +0700

    menambah container foto

--- lib/container_widget.dart
+++ lib/container_widget.dart
@@ -7,11 +7,49 @@ class BelajarContainer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: 200,
-      height: 200,
-      margin: EdgeInsets.all(10),
-      color: Colors.grey,
-      child: Text("ini adalah container"),
+      width: double.infinity,
+      height: double.infinity,
+      // margin: EdgeInsets.all(10),
+      padding: EdgeInsets.all(10),
+      decoration: BoxDecoration(
+        gradient: LinearGradient(colors: [Colors.pinkAccent, Colors.purpleAccent]),
+        // borderRadius: BorderRadius.circular(10)
+      ),
+      child: Center(
+        child: Container(
+          width: double.infinity,
+          height: double.infinity,
+          margin: EdgeInsets.all(10),
+          padding: EdgeInsets.all(20),
+          decoration: BoxDecoration(
+              gradient: LinearGradient(
+                  colors: [Colors.lightBlueAccent, Colors.pinkAccent]),
+              borderRadius: BorderRadius.circular(10)),
+          child: Container(
+            width: double.infinity,
+            height: double.infinity,
+            margin: EdgeInsets.all(10),
+            padding: EdgeInsets.all(20),
+            decoration: BoxDecoration(
+                gradient: LinearGradient(colors: [Colors.pink, Colors.purple]),
+                borderRadius: BorderRadius.circular(10)),
+            child: Container(
+              width: 500,
+              height: 500,
+              margin: EdgeInsets.all(10),
+              padding: EdgeInsets.all(20),
+              foregroundDecoration: BoxDecoration(
+                  image: DecorationImage(
+                      image: 
+                        NetworkImage("https://m.media-amazon.com/images/I/61oocbMpQML._AC_UF1000,1000_QL80_.jpg"),
+                      fit: BoxFit.fill),
+                  gradient:
+                      LinearGradient(colors: [Colors.green, Colors.brown]),
+                  borderRadius: BorderRadius.circular(10)),
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
